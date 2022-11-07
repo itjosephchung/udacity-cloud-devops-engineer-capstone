58d65f0eadf7f72fd1f84e93c2447bd087fe02b8
Joseph Chung <josephchung@Jo-MacBook-Pro.local>
2022-11-08 01:37:47 +0900

    Added circleci

--- app.py
+++ app.py
@@ -5,7 +5,5 @@ app = Flask(__name__)
 def home():
     return "<h1 style='text-align: center;'>Welcome to Cloud DevOps Engineer Capstone Project!</h1>"
 
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
